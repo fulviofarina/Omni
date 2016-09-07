e69000e2d1466f40a582f6a93ecb74ef92696348
fulviofarina <fulviofarina@gmail.com>
2016-09-07 00:06:00 -0400

    fix 1.8
    Best yet

--- VTools.Explorer/Properties/AssemblyInfo.cs
+++ VTools.Explorer/Properties/AssemblyInfo.cs
@@ -1,37 +1,36 @@
 ﻿using System.Reflection;
-using System.Resources;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Visual Tools")]
-[assembly: AssemblyDescription("Visual Tools Resources")]
+[assembly: AssemblyTitle("VTools.Explorer")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Motion Inc")]
-[assembly: AssemblyProduct("Visual Tools")]
-[assembly: AssemblyCopyright("Fulvio Farina Arboccò, Copyright (c) 2013")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VTools.Explorer")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8064dba6-1c63-409e-b540-9b02d2bc33d1")]
+[assembly: Guid("fe27c8a4-4457-48c3-878b-e20d8b8c5c1a")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: NeutralResourcesLanguageAttribute("en-US")]
